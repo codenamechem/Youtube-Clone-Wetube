206ff79a8cd28f411ccf8a64d6488603bd88bffd
OSS <fs926@naver.com>
2021-04-23 03:11:59 +0900

    address

--- src/db.js
+++ src/db.js
@@ -4,18 +4,16 @@ import mongoose from "mongoose";
 dotenv.config();
 
 mongoose.connect(
-  "mongodb://wetube:pR5w2Bv_X.gTVxA@wetube-shard-00-00.orwqd.mongodb.net:27017,wetube-shard-00-01.orwqd.mongodb.net:27017,wetube-shard-00-02.orwqd.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-61gfg5-shard-0&authSource=admin&retryWrites=true&w=majority",
+  process.env.PRODUCTION ? process.env.MONGO_URL_PROD : process.env.MONGO_URL,
   {
     useNewUrlParser: true,
     useFindAndModify: false,
-    useUnifiedTopology: true,
   }
 );
 
 const db = mongoose.connection;
 
 const handleOpen = () => console.log("✅  Connected to DB");
-
 const handleError = (error) =>
   console.log(`❌ Error on DB Connection:${error}`);
 

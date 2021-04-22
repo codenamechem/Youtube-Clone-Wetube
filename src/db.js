b8529c8805ba4a87de33bd0463c5229ab39b38ef
OSS <fs926@naver.com>
2021-04-23 02:51:57 +0900

    address

--- src/db.js
+++ src/db.js
@@ -3,15 +3,12 @@ import mongoose from "mongoose";
 
 dotenv.config();
 
-mongoose.connect(
-  process.env.PRODUCTION ? process.env.MONGO_URL_PROD : process.env.MONGO_URL,
-  {
-    useNewUrlParser: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-  }
-);
+mongoose.connect(process.env.MONGO_URL_PROD, {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
 
 const db = mongoose.connection;
 

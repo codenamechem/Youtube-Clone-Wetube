ee681c12cb106d26128cfa75c44dd9f15bd804c1
OSS <fs926@naver.com>
2021-04-23 02:54:44 +0900

    address

--- src/db.js
+++ src/db.js
@@ -3,12 +3,15 @@ import mongoose from "mongoose";
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO_URL_PROD, {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-});
+mongoose.connect(
+  "mongodb://wetube:pR5w2Bv_X.gTVxA@wetube-shard-00-00.orwqd.mongodb.net:27017,wetube-shard-00-01.orwqd.mongodb.net:27017,wetube-shard-00-02.orwqd.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-61gfg5-shard-0&authSource=admin&retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  }
+);
 
 const db = mongoose.connection;
 
